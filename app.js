8a15283b2128d571872d589c54a581d7b0186d2f
Dipankar498 <barman498@gmail.com>
2020-06-21 14:16:26 +0530

    handlebars added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
+const exphbs = require('express-handlebars');
 const connectDB = require('./config/db');
 
 // Load config
@@ -12,9 +12,14 @@ connectDB();
 
 const app = express();
 
+// Logging
 if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
+
+// Handlebars Setup
+app.engine('.hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }));
+app.set('view engine', '.hbs');
 
 const PORT = process.env.PORT || 5000;
 
